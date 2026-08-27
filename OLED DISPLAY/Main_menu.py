4f2d6f12d2660bd67150b9a2f596d9eda16c1a32
ARYA-astro <paisemilengenahi@gmail.com>
2026-08-27 23:40:26 +0530

    Refined the code and added limits to the number of options displayed on the OLED screen. The menu now only shows a maximum of 5 options at a time, and the selection rectangle is correctly drawn around the selected option.

--- OLED DISPLAY/Main_menu.py
+++ OLED DISPLAY/Main_menu.py
@@ -23,13 +23,28 @@ sel_idx = 0
 sel_opt = "Menu"
 
 def draw_menu(title):
+    len_options = len(main_menu[sel_opt])
+    if len_options <= 3:
+        start_idx = 0
+        end_idx = len_options
+    else:
+        if sel_idx == 0:
+            start_idx = 0
+        elif sel_idx == len_options - 1:
+            start_idx = len_options - 3
+        else:
+            start_idx = sel_idx - 1
+
+        end_idx = start_idx + 3            
+                
     with canvas(device) as draw:
         draw.rectangle((0, 0, 127, 63), outline="white", fill="black")  # Clear the screen
         draw.text((37, 3), title, fill="white", font=FONT)
-        for index, option in enumerate(main_menu[sel_opt]):
+        for index, idx in enumerate(range(start_idx, end_idx)):
+            option = main_menu[sel_opt][idx]
             y_position = 14 + index * 12
             rectangle_coords = (1, y_position, 127, y_position + 11)
-            if index == sel_idx:
+            if idx == sel_idx:
                 draw.rectangle(rectangle_coords, outline="black", fill="white")
                 draw.text((5, y_position), option, fill="black", font=FONT)
             else:
