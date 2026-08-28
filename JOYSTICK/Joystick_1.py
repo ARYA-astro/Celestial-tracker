e93746203bb1e0807d2a4d63c6a2803f6d3a4589
Hardik Arya <paisemilengenahi@gmail.com>
2026-08-28 23:23:11 +0530

    Enhance joystick input handling with center detection
    Refactor joystick handling to include center position and button state.

--- JOYSTICK/Joystick_1.py
+++ JOYSTICK/Joystick_1.py
@@ -8,18 +8,27 @@ select_button = gpio.DigitalInputDevice(22, pull_up=True)
 
 while True:
     if x_axis.value == 1:
-        print("Joystick:HIGH")
+        x_val = "RIGHT"
     elif x_axis.value == 0:
-        print("Joystick:LOW")
-    elif y_axis.value == 1:
-        print("Joystick:HIGH")    
-    elif y_axis.value == 0:
-        print("Joystick:LOW")
-    elif select_button.value == 1:
-        print("Button:HIGH")
-    elif select_button.value == 0:
-        print("Button:LOW")
+        x_val = "LEFT"
+    else:
+        x_val = "CENTRE"
 
+    if y_axis.value == 1:
+        y_val = "UP"
+    elif y_axis.value == 0:
+        y_val = "DOWN"
+    else:
+        y_val = "CENTRE"
+    
+    btn_val = "YES" if select_button.value == 1 else "NO"
+
+    if x_val == "CENTRE" and y_val == "CENTRE":
+        ovr_val = "CENTRE"
+    else:
+        ovr_val = f"{y_val}-{x_val}".strip("CENTRE")
+    print(f"Direction: {ovr_val:<12} | Button: {btn_val}")    
+    
         time.sleep(0.1)
             
 
